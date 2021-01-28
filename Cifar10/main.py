123c4a74656925a8166f912834a4002ab167b8e5
fengyemuse <jieyuzhao@hotmail.com>
2021-01-28 16:51:57 +0800

    cifar10

--- Cifar10/main.py
+++ Cifar10/main.py
@@ -3,6 +3,18 @@ from tensorflow.keras.datasets import cifar10
 from tensorflow.keras import backend
 from tensorflow.keras.initializers import RandomNormal
 from tensorflow.keras.callbacks import LearningRateScheduler, TensorBoard
+import numpy as np
+#
+# from tensorflow.compat.v1 import ConfigProto
+# from tensorflow.compat.v1 import InteractiveSession
+#
+# config = ConfigProto()
+# config.gpu_options.allow_growth = True
+# session = InteractiveSession(config=config)
+
+physical_devices = tf.config.experimental.list_physical_devices('GPU')
+assert len(physical_devices) > 0, "Not enough GPU hardware devices available"
+tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
 backend.set_image_data_format('channels_first')
 
@@ -91,16 +103,17 @@ def build_model():
     model.add(tf.keras.layers.Activation('softmax'))
     model.summary()
     model.compile(loss='categorical_crossentropy',
-                  optimizer=tf.keras.optimizers.SGD(lr=0.1, momentum=0.9, nesterov=True),
+                  optimizer=tf.keras.optimizers.SGD(lr=0.01, momentum=0.9, nesterov=True),
                   metrics=['accuracy'])
     return model
 
 
 if __name__ == '__main__':
     (x_train, y_train), (x_validation, y_validation) = cifar10.load_data()
-
-    y_train = tf.keras.utils.to_categorical(y_train, num_classes)
-    y_validation = tf.keras.utils.to_categorical(y_validation, num_classes)
+    print(np.isnan(x_validation).any())
+    print(np.isnan(y_validation).any())
+    y_train = tf.keras.utils.to_categorical(y_train)
+    y_validation = tf.keras.utils.to_categorical(y_validation)
     x_train, x_validation = normalize_preprocessing(x_train, x_validation)
     model = build_model()
     tb_cb = TensorBoard(log_dir=log_filepath, histogram_freq=0)
